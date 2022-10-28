5c17da0915b019d7e5f28262a724f6ad58995518
kogotag <kogotag@mail.ru>
2022-10-28 20:51:41 +0400

    lections profcult

--- media.php
+++ media.php
@@ -18,10 +18,13 @@
                 <li class="breadcrumb-item active">Медиа</li>
             </ol>
         </nav>
-        
-        <div>
+
+        <div class="container-fluid px-2 mb-4">
             <a href="/media">Все аудио</a><br>
             <a href="https://www.youtube.com/playlist?list=PL4f6-h4zVq29EyYmdNL518uwOgdtZzWuA">Записи лекций по истории</a><br>
+            <?php if (isLoggedIn() && $_SESSION["user_from_group"] === 1): ?>
+                <a href="https://www.youtube.com/playlist?list=PLcsiGEUf6YIH4Vi0dDqD3f-BKpoZ4QXYu">Записи лекций по профкультуре</a><br>
+            <?php endif; ?>
         </div>
 
         <?php require 'footer.php'; ?>
