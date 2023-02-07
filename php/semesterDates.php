8406d86c8584b3a772baeca966ba9cee3c2e8ad7
kogotag <kogotag@mail.ru>
2023-02-07 18:49:34 +0400

    fixed dates in navbar/ssau_schedule

--- php/semesterDates.php
+++ php/semesterDates.php
@@ -20,17 +20,8 @@ for ($i = 1; $i <= $SM_semesters_count; $i++) {
     }
 }
 
-$SM_semester_auto = intval($SM_date_now->format("Y")) - 2021;
-$SM_semester_start = DateTime::createFromFormat("d.m.Y", "01.09." . ($SM_semester_auto + 2021));
-$SM_first_september_weekday = $SM_semester_start->format("N");
-$SM_date_first_monday = clone $SM_semester_start;
-$SM_date_first_monday->modify("-" . $SM_first_september_weekday - 1 . " days");
-
-// old: used in navbar
-$SM_week = floor($SM_date_now->diff($SM_date_first_monday)->days / 7) + 1;
-$SM_day = $SM_date_now->diff($SM_date_first_monday)->days - ($SM_week - 1) * 7 + 1;
-
-//
+$SM_week = getWeeksFromSemesterStart($SM_current_semester);
+$SM_day = $SM_date_now->diff(getSemesterFirstMonday($SM_current_semester))->days - ($SM_week - 1) * 7 + 1;
 
 function getWeeksFromSemesterStart($semester) {
     global $SM_date_now;
