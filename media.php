bad07ffb9a8850b6b8a193493ec6679353a6ebc3
kogotag <kogotag@mail.ru>
2023-02-14 16:41:56 +0400

    media books

--- media.php
+++ media.php
@@ -21,18 +21,25 @@
         </nav>
 
         <div class="container-fluid px-2 mb-4">
-            <h3>Семестр 1</h3>
-            <a href="/media">Все аудио</a><br>
             <a href="https://www.youtube.com/playlist?list=PL4f6-h4zVq29EyYmdNL518uwOgdtZzWuA">Записи лекций по истории</a><br>
             <?php if (isLoggedIn() && $_SESSION["user_from_group"] === 1): ?>
                 <a href="https://www.youtube.com/playlist?list=PLcsiGEUf6YIH4Vi0dDqD3f-BKpoZ4QXYu">Записи лекций по профкультуре</a><br>
             <?php endif; ?>
-            <h3 class="mt-4">Семестр 2</h3>
             <?php if (isLoggedIn() && $_SESSION["user_from_group"] === 1): ?>
             <a href="https://drive.google.com/drive/folders/1SdMb3JlT97Ov8xnfqVD9V0agIEz9gOWF">Программирование гугл-диск</a>
             <?php else: ?>
-            <a href="/errorPage.php?message=authLink">программирование гугл-диск</a>
+            <a href="/errorPage.php?message=authLink">Программирование гугл-диск</a>
             <?php endif; ?>
+            <h3>Механика</h3>
+            <h4>Учебники</h4>
+            <a href="/media/physics_uchebnik_irodov.pdf">Учебник Иродова</a><br>
+            <a href="/media/physics_uchebnik_matveev.pdf">Учебник Матвеева</a><br>
+            <a href="/media/physics_uchebnik_saveliev.pdf">Учебник Савельева</a><br>
+            <a href="/media/physics_uchebnik_sivuhin.pdf">Учебник Сивухина</a><br>
+            <a href="/media/physics_uchebnik_trofimova.pdf">Учебник Трофимова</a>
+            <h4>Задачники</h4>
+            <a href="/media/physics_zadachnik_chertov.pdf">Задачник Чертова</a><br>
+            <a href="/media/physics_zadachnik_irodov.pdf">Задачник Иродова</a>
         </div>
 
         <?php require 'footer.php'; ?>
