bf908ccbf6569fa84ceec005e9b4541f798fb048
kogotag <kogotag@mail.ru>
2023-04-01 22:56:22 +0400

    1st april fools

---
+++ integrals.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>1105 Сайт</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="icon" type="image/x-icon" href="img/logo.svg">
+    </head>
+    <body class="bg-light">
+        <p>В этой статье я разберу все интегралы, которые были заданы на матанализе...</p>
+        <?php require 'php/importImportantJsScripts.php'; ?>
+    </body>
+</html>
